2c5e359dd794601df82965d88af461d4320d9d15
Jian Li <pyguni@gmail.com>
2019-03-06 10:14:59 +0900

    Break long string into multiple lines

--- recovery.sh
+++ recovery.sh
@@ -34,56 +34,64 @@ _EOF_
 
 [ "$1" = "-h" ] || [ "$1" = '-?' ] && _usage && exit 0
 
-######################################################################
+################################################################################
 # Checks whether the SONA apps are activated.
 #
 # Arguments:
 #   onos_ip: ONOS IP address
 # Returns:
-#   boolean: true(0) if the SONA apps are activated, false(1) otherwise
-######################################################################
+#   None
+################################################################################
 function check_sona_app () {
-  check_str='curl -sL --user $ONOS_USER:$ONOS_PASSWORD -w "%{http_code}\\n" "http://$1:8181/onos/openstacknetworking/management/floatingips/all" -o /dev/null'
+  check_str='curl -sL --user $ONOS_USER:$ONOS_PASSWORD -w "%{http_code}\\n" '
+  check_str+='"http://$1:8181/onos/openstacknetworking/management/floatingips/all" '
+  check_str+='-o /dev/null'
   eval $check_str
 }
 
-######################################################################
+################################################################################
 # Obtains the k8s POD IP with given POD name.
 #
 # Arguments:
 #   pod_name: k8s POD name
 # Returns:
 #   pod_ip: k8s POD IP address mapped with the given POD name
-######################################################################
+################################################################################
 function get_pod_ip () {
   kubectl get po $1 -o wide -n openstack | awk 'FNR == 2 {print $6}'
 }
 
-######################################################################
+################################################################################
 # Synchronizes the openstack states with SONA.
 #
 # Arguments:
 #   onos_ip: ONOS node IP address
 # Returns:
 #   None
-######################################################################
+################################################################################
 function sync_states () {
-  curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET http://$1:8181/onos/openstacknetworking/management/sync/states -o /dev/null
+  curl_str='curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET '
+  curl_str+='http://$1:8181/onos/openstacknetworking/management/sync/states '
+  curl_str+='-o /dev/null'
+  eval $curl_str
 }
 
-######################################################################
+################################################################################
 # Installs flow rules into OVS by referring to openstack states.
 #
 # Arguments:
 #   onos_ip: ONOS node IP address
 # Returns:
 #   None
-######################################################################
+################################################################################
 function sync_rules () {
-  curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET http://$1:8181/onos/openstacknetworking/management/sync/rules -o /dev/null
+  curl_str='curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET '
+  curl_str+='http://$1:8181/onos/openstacknetworking/management/sync/rules '
+  curl_str+='-o /dev/null'
+  eval $curl_str
 }
 
-######################################################################
+################################################################################
 # Configs the default ARP mode.
 #
 # Arguments:
@@ -91,80 +99,89 @@ function sync_rules () {
 #   arp_mode: ARP mode (broadcast | proxy)
 # Returns:
 #   None
-######################################################################
+################################################################################
 function config_arp_mode () {
-  curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET http://$1:8181/onos/openstacknetworking/management/config/arpmode/$2 -o /dev/null
+  curl_str='curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET '
+  curl_str+='http://$1:8181/onos/openstacknetworking/management/config/arpmode/$2 '
+  curl_str+='-o /dev/null'
+  eval $curl_str
 }
 
-######################################################################
+################################################################################
 # Deletes k8s PODs.
 #
 # Arguments:
 #   pod_list: space separated pod list
 # Returns:
 #   None
-######################################################################
+################################################################################
 function delete_pod () {
   kubectl delete po $1 -n openstack
 }
 
-######################################################################
+################################################################################
 # Checks k8s POD status.
 #
 # Arguments:
 #   pod_name: pod name
 # Returns:
 #   None
-######################################################################
+################################################################################
 function check_pod_status () {
   kubectl get po $1 -n openstack | awk 'FNR == 2 {print $3}'
 }
 
-######################################################################
+################################################################################
 # Checks k8s POD existence.
 #
 # Arguments:
 #   pod_name: pod name
 # Returns:
 #   None
-######################################################################
+################################################################################
 function check_pod_existence () {
   kubectl get po -n openstack | grep $1 | awk '{print $1}'
 }
 
-######################################################################
+################################################################################
 # Backups openstack node configuration.
 #
 # Arguments:
 #   onos_ip: ONOS IP address
 # Returns:
 #   None
-######################################################################
+################################################################################
 function backup_node_config () {
   rm -rf $SONA_CONF_FILE
-  curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET http://$1:8181/onos/openstacknode/configure -o $SONA_CONF_FILE > /dev/null
+  curl_str='curl -s --user $ONOS_USER:$ONOS_PASSWORD -X GET '
+  curl_str+='http://$1:8181/onos/openstacknode/configure '
+  curl_str+='-o $SONA_CONF_FILE > /dev/null'
+  eval $curl_str
 }
 
-######################################################################
+################################################################################
 # Restores openstack node configuration.
 #
 # Arguments:
 #   onos_ip: ONOS IP address
 # Returns:
 #   None
-######################################################################
+################################################################################
 function restore_node_config () {
-  curl -s --user $ONOS_USER:$ONOS_PASSWORD -X POST -H "Content-Type: application/json" http://$1:8181/onos/openstacknode/configure -d @$SONA_CONF_FILE
+  curl_str='curl -s --user $ONOS_USER:$ONOS_PASSWORD '
+  curl_str+='-X POST -H "Content-Type: application/json" '
+  curl_str+='http://$1:8181/onos/openstacknode/configure -d @$SONA_CONF_FILE'
+  eval $curl_str
 }
 
-######################################################################
+################################################################################
 # Checks SONA node configuration backup file.
 #
 # Arguments:
 #   None
 # Returns:
 #   None
-######################################################################
+################################################################################
 function check_backup_file () {
   if [[ -f "$SONA_CONF_FILE" ]] && [[ -s "$SONA_CONF_FILE" ]]
   then
@@ -235,7 +252,6 @@ function main () {
   for pod_ip in "${pod_ip_list[@]}"; do
     while true
     do
-      # check_sona_app_res=$(eval check_sona_app $pod_ip)
       check_sona_app_str='check_sona_app $pod_ip'
       if [ $(eval $check_sona_app_str) == "200" ];
       then
@@ -247,16 +263,16 @@ function main () {
     echo "SONA apps at $pod_ip are activated!"
   done
 
-  echo "== Restore SONA configuration for ${pod_ip_list[0]} =="
+  echo "== Restore SONA configuration at ${pod_ip_list[0]} =="
   restore_node_config ${pod_ip_list[0]}
 
-  echo "== Configure ARP broadcast mode for ${pod_ip_list[0]} =="
+  echo "== Configure ARP broadcast mode at ${pod_ip_list[0]} =="
   config_arp_mode ${pod_ip_list[0]} $ARP_MODE
 
-  echo "== Synchronize openstack states for ${pod_ip_list[0]} =="
+  echo "== Synchronize openstack states at ${pod_ip_list[0]} =="
   sync_states ${pod_ip_list[0]}
 
-  echo "== Synchronize openflow rules for ${pod_ip_list[0]} =="
+  echo "== Synchronize openflow rules at ${pod_ip_list[0]} =="
   sync_rules ${pod_ip_list[0]}
 
   rm -rf $SONA_CONF_FILE
